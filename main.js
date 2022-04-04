a943787f39b86e6bec1ab5625927c5af66a65bd7
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-04-04 17:09:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+value_from_input_box = "";
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+}
+
+function start()
+{
+    objectDetctor = ml5.objectDetctor('cocossd',modelLoaded);length
+    document.getElementById("status").innerHTML = "Status: Detecting Objects ";
+    value_from_input_box = document.getElementById("input_box").value;
+}
+function modelLoaded()
+{
+    console.log("Model Loaded! ");
+    status = true;
+}
+
+function draw()
+{
+image(video, 0,0,300,300);
+}
